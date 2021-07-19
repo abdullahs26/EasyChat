f0ec763a9205b1c465162c556a88bd183cd47a92
abdullahs26 <abdullahshahid20022002@gmail.com>
2021-07-19 16:01:46 -0400

    updated ChatFeed, MessageForm, MyMessage, and TheirMessage components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="e10de705-9161-4bf2-935e-b59e91e5bc65"
+            userName="testuser"
+            userSecret="test123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+        />
+    );
+}
+
+export default App; 
